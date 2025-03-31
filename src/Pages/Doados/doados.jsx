a1692a8e55a393cf356cf683e88afe380385b22b
Ad3Agra90 <ad.agra90@gmail.com>
2025-03-31 15:37:03 -0300

    Página doados - implementando API

--- src/Pages/Doados/doados.jsx
+++ src/Pages/Doados/doados.jsx
@@ -1,48 +1,36 @@
 import exemplo from '../../assets/img/exemplo_book.png'
 import S from './doados.module.scss'
-
+import axios from 'axios'
+import {useState, useEffect} from 'react'
 
 export default function Doados(){
+
+    const [livros, setLivros] = useState([])
+    
+    const getLivros = async () => {
+        const response = await axios.get("https://modulo2-desafio02.onrender.com/livros")
+        setLivros(response.data)
+    }
+    useEffect(() => {
+        getLivros()
+    }, [])
+
+
+
+
     return(
         <section>
         <section className={S.doados}>
             <h2 className={S.title_doados}>Livros Doados</h2>
             <section className={S.grid_doados}>
-                <article>
-                    <img src={exemplo} alt="exemplo" />
-                    <h3 className={S.book_title}>O protagonista</h3>
-                    <p>Susanne Andrade
-                    </p>
-                    <p>Ficção</p>
+                {livros.map((item) =>(
+                <article key={item.id}>
+                    <img src={item.imagem_url} alt="" />
+                    <h3 className={S.book_title}>{item.titulo}</h3>
+                    <p>{item.autor}</p>
+                    <p>{item.categoria}</p>
                 </article>
-                <article>
-                    <img src={exemplo} alt="exemplo" />
-                    <h3 className={S.book_title}>O protagonista</h3>
-                    <p>Susanne Andrade
-                    </p>
-                    <p>Ficção</p>
-                </article>
-                <article>
-                    <img src={exemplo} alt="exemplo" />
-                    <h3 className={S.book_title}>O protagonista</h3>
-                    <p>Susanne Andrade
-                    </p>
-                    <p>Ficção</p>
-                </article>
-                <article>
-                    <img src={exemplo} alt="exemplo" />
-                    <h3 className={S.book_title}>O protagonista</h3>
-                    <p>Susanne Andrade
-                    </p>
-                    <p>Ficção</p>
-                </article>
-                <article>
-                    <img src={exemplo} alt="exemplo" />
-                    <h3 className={S.book_title}>O protagonista</h3>
-                    <p>Susanne Andrade
-                    </p>
-                    <p>Ficção</p>
-                </article>
+                ))}
             </section>
         </section>
         </section>
